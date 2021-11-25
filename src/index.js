d0d5a1984dde05d23f055488bc4ec2ba7138af5f
K-RAMYA05 <ramyak.2019@vitstudent.ac.in>
2021-11-25 11:42:41 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(express.static(publicPath))
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.render('index');
   });
 app.get('/login',(req,res)=>{
