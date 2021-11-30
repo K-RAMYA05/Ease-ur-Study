638b30c274cf155a0cbc88d7e07c625061f4f3e5
K-RAMYA05 <ramyak.2019@vitstudent.ac.in>
2021-11-30 09:36:51 +0530

    make it better

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
-const dbURI = "mongodb+srv://k01:k01ramya@cluster0.zplgs.mongodb.net/details?retryWrites=true&w=majority";
+const dbURI = "mongodb+srv://k01:pass1@cluster0.zplgs.mongodb.net/details?retryWrites=true&w=majority";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
