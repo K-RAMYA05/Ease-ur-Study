a7599f1bd7b29e06f526e3e68a580f4c7e2159dc
HP <HP@Mac.lan>
2025-03-21 12:47:19 -0700

    new

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbURI = "mongodb+srv://Admin:<db_password>@cluster0.sltih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dbURI = "mongodb+srv://Admin:Admin@cluster0.sltih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(dbURI, {
   useNewUrlParser: true,
