2cbfff555af3c75bafa00311ec2bf483ab803459
HP <HP@Mac.lan>
2025-03-21 13:23:18 -0700

    new

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-const dbURI = "mongodb+srv://Admin:Admin@cluster0.sltih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const dbURI = "mongodb://Admin:Admin@cluster0.sltih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true";
 
 mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   ssl: true,
   tls: true,
-  tlsInsecure: true, // Bypass certificate validation (use with caution)
+  tlsAllowInvalidCertificates: false,
+  sslValidate: true
 })
 .then(() => console.log("Connected successfully"))
 .catch(err => console.error("Connection error:", err));
+
